6423c7df4ca951a703bb6067fb3f8e01fb8e48a7
Nicolas Bland <nicolas.bland@protonmail.com>
2020-12-02 23:16:25 +0100

    admin fichier à partager : aide à la saisie du lien

--- src/cms/preview-templates/FilesPreview.js
+++ src/cms/preview-templates/FilesPreview.js
@@ -3,8 +3,10 @@ import PropTypes from "prop-types"
 
 const FilesPreview = ({ entry }) => (
   <>
-    <h1>Lien à copier/coller :</h1>
+    <h2>Lien à copier/coller dans les articles (mode édition "Texte enrichi"):</h2>
     <a href={entry.getIn(["data", "file"])}>{entry.getIn(["data", "title"])}</a>
+    <h2>Ou pour que le lien s'ouvre dans un nouvel onglet, se placer en mode édition "Markdown" et copier le code suivant :</h2>
+    <code>&lt;a href="{entry.getIn(["data", "file"])}" target="_blank"&gt;{entry.getIn(["data", "title"])}&lt;/a&gt;</code>
   </>
 )
 

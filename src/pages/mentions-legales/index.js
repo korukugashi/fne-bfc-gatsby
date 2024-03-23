c2d90d20cd637deff53dd07d227b8960ad445524
Nicolas B <korukugashi@users.noreply.github.com>
2024-03-23 21:57:46 +0100

    Update index.js
    changement adresse siège

--- src/pages/mentions-legales/index.js
+++ src/pages/mentions-legales/index.js
@@ -19,9 +19,7 @@ const MentionsLegales = () => (
                 N° SIRET : 	315 564 542 000 43 - Code APE : 9499Z
             </p>
             <p>
-                Maison de l'environnement de Bourgogne Franche-Comté
-                <br />
-                7 rue Voirin 25000 Besançon / Tél. 03 81 80 92 98
+                30-32 avenue fontaine argent 25000 Besançon / Tél. 03 81 80 92 98
             </p>
             <h2>Hébergeur</h2>
             <p>

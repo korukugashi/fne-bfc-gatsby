2fff2b41afc06395dafde42634f62661ffea97d7
Nicolas B <korukugashi@users.noreply.github.com>
2024-03-23 21:57:05 +0100

    Update index.js
    changement adresse siège

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -23,13 +23,13 @@ const Contact = () => (
                 <div className="container">
                     <div className="columns is-vcentered">
                         <address className="column is-6">
-                            <p><small>C/O</small> Maison de l’environnement</p>
-                            <p>7 rue Voirin<br />25000 Besançon</p>
+                            <p>30-32 avenue fontaine argent<br />25000 Besançon</p>
                             <div><span className="has-text-weight-bold">Tél :</span> 03 81 80 92 98</div>
                             <div><span className="has-text-weight-bold">Email :</span> <Obfuscate email="contact@fne-bfc.fr" /></div>
                         </address>
                         <div className="column is-6">
-                            <iframe title="Adresse FNE BFC" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" style={{ width: '100%', height: 300}} src="https://www.openstreetmap.org/export/embed.html?bbox=6.012308299541474%2C47.24160465865322%2C6.014362871646881%2C47.24270633650406&amp;layer=mapnik&amp;marker=47.24215641091987%2C6.013335585594177"></iframe>
+                            <iframe src="https://www.openstreetmap.org/export/embed.html?bbox=6.033417284488679%2C47.24577270430456%2C6.035471856594087%2C47.24685062516591&amp;layer=mapnik&amp;marker=47.246311667477116%2C6.034444570541382" title="Adresse FNE BFC" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" style={{ width: '100%', height: 300}}></iframe><br/>
+                            <small><a href="https://www.openstreetmap.org/?mlat=47.24631&amp;mlon=6.03444#map=19/47.24631/6.03444">Afficher une carte plus grande</a></small>
                         </div>
                     </div>
                 </div>
